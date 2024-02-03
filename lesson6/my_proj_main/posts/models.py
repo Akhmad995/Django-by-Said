83d8fe3de174f133e78475e0122c98507bf0cec8
Akhmad995 <axmed-005@mail.ru>
2024-02-03 19:13:13 +0300

    add lesson6

---
+++ lesson6/my_proj_main/posts/models.py
@@ -0,0 +1,106 @@
+# Модели
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+RATING = [(1, 'Плохо'), (2, 'Посредственно'), (3, 'Нормально'), (4, 'Хорошо')]
+
+
+# Модель пользователя встроена в Django и импортируется следующим образом:
+User = get_user_model()
+
+
+# Модель тегов
+class Tag(models.Model):
+    name = models.CharField(max_length=15, unique=True, null=False)
+
+
+# Модель публикаций
+class Post(models.Model):
+    # Поля модели
+    # Возможные типы полей - IntegerField, FloatField, EmailField, FileField,
+    # ImageField, BooleanField, и т.д.
+
+    # Возможные аргументы полей:
+    # - max_length (максимальное длина значения поля),
+    # - unique (значение записи в таблице уникально),
+    # - blank (разрешено пустое поле при заполнении формы),
+    # - null (разрешено хранение пустого значения NULL),
+    # - default (полю задается значение по умолчанию),
+    # - auto_now_add (задаются текущие дата и время при добавлении записи),
+    # - auto_now (задаются текущие дата и время при каждом изменении записи),
+    # - choices (задается в виде списка определенный набор вариантов для поля),
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=False,
+        null=False,
+        help_text="Заголовок",
+        verbose_name="Заголовок публикации"
+    )
+    text = models.TextField(
+        blank=False,
+        verbose_name="Текст публикации"
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    # Внешний ключ для реализации отношения один-ко-многим.
+    # Аргумент related_name задает имя, по которому можно получить
+    # у определенного объекта все зависимые объекты данной модели.
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name="Автор публикации"
+    )
+    
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='posts',
+        through='PostTag',
+        verbose_name="Теги публикации"
+    )
+
+    # Класс Meta задает доп. установки данной модели.
+    class Meta:
+        # ordering - переопределяет поле сортировки записей в модели.
+        # Знак "-" перед именем поля изменяет сортировку на убывающую.
+        ordering = ['-pub_date']
+        verbose_name="Публикация"
+        verbose_name_plural="Публикации"
+
+    def __str__(self):
+        return f'{self.title[:30]} -- {self.pub_date}'
+
+
+
+# Промежуточная модель для связи Многие-ко-многим
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+# Модель комментариев
+class Comments(models.Model):
+    comment = models.TextField(blank=True, null=True)
+
+
+# Модель отзывов
+class Review(models.Model):
+    title = models.CharField(max_length=100, help_text="Заголовок")
+    rating = models.IntegerField(choices=RATING)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        blank=False,
+        null=False
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    # Зависимость Один-к-одному
+    comments = models.OneToOneField(
+        Comments,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='review'
+    )
---
+++ lesson6/my_proj_main/posts/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import PostForm
+from .models import Post
+from .models import User
+
+def index(request):
+    return HttpResponse('Главная')
+
+
+def posts_list(request):
+    # CRUD
+
+    # получение записи по первичному ключу
+    user = User.objects.get(pk=1)
+    
+    # Создание записи
+    # post = Post.objects.get_or_create(title='Заголовок 3', text='Длинный текст 3', author=user)
+    post = Post.objects.get(id=3)
+
+    # Обновление
+    post.text = 'Длинный текст 4 - обновление'
+    post.save()
+
+    # Удаление
+    # post.delete()
+    
+    # Получаем список записей
+    posts = Post.objects.all()
+
+    # Получение части публикаций
+    # posts = Post.objects.exclude(title='Название заголовка')
+
+    # Метод filter
+    # posts = Post.objects.filter(title='Название заголовка')
+    # posts = Post.objects.filter(author=user)
+    
+    # contains - содержит подстроку
+    # posts = Post.objects.filter(text__contains='обнов')
+    # startwith, endwith - поле начинается*заканчивается
+    
+    # in - вхождение в набор значений 
+    # posts = Post.objects.filter(title__in=('Заг1', 'Заг2'))
+
+    # range - вхождение в диапозон/интервал
+    # posts = Post.objects.filter(id__range=(1, 5))
+
+    # gt, gte, lt, lte - сравнение значения поля >, >=, <, <=
+    # posts = Post.objects.filter(id__gte=4)
+
+    # year, month, day, week, week_day, time, hour, minute, second
+    # Проверка поля по совпападению по отдельному из значений
+    # posts = Post.objects.filter(pub_date__hour=17)
+
+    # Комбинирование
+    # posts = Post.objects.filter(pub_date__hour__gte=16)
+    # posts = Post.objects.filter(pub_date__hour=16).filter(text__endwith='обновл')
+
+    # По полям связанных объектов
+    # posts = Post.objects.filter(author__username='Admin')
+    # print( Post.objects.filter(author__username='Admin').query )
+
+    # Сортировка объектов
+    # posts = Post.objects.filter(author__username='Admin').order_by('-pub_date')
+    # posts = Post.objects.filter(author__username='Admin').order_by('-pub_date')[:3] # Срез возвращает набор данных - первые 3
+    
+    # исключение отдельных полей
+    # posts = Post.objects.defer('text).all()
+    
+    # count = Post.objects.count()
+
+    # Агрегирующие функции
+    # posts = Post.objects.aggregate( Max('id) )
+    # posts = Post.objects.aggregate( Avg('id) )
+                               
+    # Получаем среднее число айдишников всех постов пользователям
+    # annotate - расширение записей доп. полями как результат вычислений
+    # users =  User.objects.annotate( posts_avg=Avg('posts__id') )
+    # for elem in users:
+    #    print(elem.posts_avg)
+
+
+    context = {'current_id': 5, 'form': PostForm, 'posts': posts}
+    return render(request, 'posts/posts_list.html', context)
+
+
+def posts_detail(request, id):
+    post = Post.objects.get_or_404(id = id)
+    context = {'post': post}
+    return render(request, 'posts/posts_detail.html', context)
