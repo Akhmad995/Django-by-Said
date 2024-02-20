c34ba2942db57a99d82464f0f46ccfd9a317edfa
Akhmad995 <axmed-005@mail.ru>
2024-02-20 21:02:13 +0300

    update lesson11

--- lesson11/my_edu_project/posts/serializers.py
+++ lesson11/my_edu_project/posts/serializers.py
@@ -3,18 +3,43 @@ from .models import Post
 
 
 
+# class PostSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=150)
+#     text = serializers.CharField()
+#     pub_date = serializers.DateTimeField(read_only=True)
+
+#     def create(self, validated_data):
+#         return Post.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.text = validated_data.get('text', instance.text)
+
+#         return instance
+
+
+
 class PostSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=150)
-    text = serializers.CharField()
-    pub_date = serializers.DateTimeField()
+    post_title = serializers.CharField(source='title')
+    pub_date = serializers.DateTimeField(read_only=True)
 
-    def create(self, validated_data):
-        return Post(**validated_data)
+    def to_representation(self, instance):
+        # return super().to_representation(instance)
+        representation =  super().to_representation(instance)
+        representation['pub_date'] = instance.pub_date.strftime('%d-%m-%Y')
+        return representation
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.text = validated_data.get('text', instance.text)
+    # def create(self, validated_data):
+    #   pas
 
-        return instance
+    # def update(self, instance, validated_data):
+    #   pass
 
+    # def to_internal_value(self, data):
+    #     data['pub_date'] = 
+    #     return super().to_internal_value(data)
 
+    class Meta:
+        model = Post
+        # fields = '__all__'
+        fields = ('post_title', 'text', 'pub_date')
--- lesson11/my_edu_project/posts/views.py
+++ lesson11/my_edu_project/posts/views.py
@@ -1,22 +1,50 @@
-from rest_framework import generics
-from rest_framework import filters
-from rest_framework import permissions
+# from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-
-    serializer_class = PostSerializer
-    filter_backends = [filters.SearchFilter]
-    search_field = ['title']
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+    
 
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
-    serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated)
 
+class PostListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+
+        print( 'serializer', serializer, type(serializer), sep='   |   ' )
+        print('****')
+        print( 'serializer.initial_data', serializer.initial_data, type(serializer.initial_data), sep='   |   ' )
+        print('****')
+
+        if serializer.is_valid():
+            print( 'serializer', serializer, type(serializer), sep='   |   ' )
+            print('****')
+            print( 'serializer.initial_data', serializer.initial_data, type(serializer.initial_data), sep='   |   ' )
+            print('****')
+
+            serializer.save()
+
+            print( 'serializer', serializer, type(serializer), sep='   |   ' )
+            print('****')
+            print( 'serializer.data', serializer.data, type(serializer.data), sep='   |   ' )
+            print('****')
+            print( 'serializer.initial_data', serializer.initial_data, type(serializer.initial_data), sep='   |   ' )
+            print('****')
+            
+            return Response(serializer.data, status=201)
+        return Response(serializer.error, status=400)
