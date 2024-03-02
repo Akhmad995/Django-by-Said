c167f0cb120add6a430b0ca8af5a89bdc17fb08a
Akhmad995 <axmed-005@mail.ru>
2024-03-02 19:10:25 +0300

    add lesson14

---
+++ lesson14/my_edu_project/posts/serializers.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+from rest_framework import serializers
+from .models import Post
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'title', 'text', 'author'
+        )
+
+
---
+++ lesson14/my_edu_project/posts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+    # path('posts/', views.post_list, name='posts_list'),
+    # path('posts/<int:pk>/', views.post_detail, name='posts_detail'),
+
+    # path('posts/', views.APIPostList.as_view(), name='posts_list'),
+    # path('posts/<int:pk>/', views.APIPostDetail.as_view(), name='posts_detail'),
+
+    # path('posts/', views.PostList.as_view(), name='posts_list'),
+    # path('posts/<int:pk>/', views.PostDetail.as_view(), name='posts_detail'),
+
+    path('posts/', views.PostList2.as_view(), name='posts_list'),
+    path('posts/<int:pk>/', views.PostDetail2.as_view(), name='posts_detail'),
+]
