8d0584cbecd001fe79be22ba7b1353d1aa5049aa
Akhmad995 <axmed-005@mail.ru>
2024-03-02 21:20:02 +0300

    update lesson14

--- lesson14/my_edu_project/posts/urls.py
+++ lesson14/my_edu_project/posts/urls.py
@@ -1,19 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import  DefaultRouter, SimpleRouter
 from . import views
 
+router = DefaultRouter()
+# router = SimpleRouter()
+
+router.register( r'posts', views.PostViewSet )
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-
-    # path('posts/', views.post_list, name='posts_list'),
-    # path('posts/<int:pk>/', views.post_detail, name='posts_detail'),
-
-    # path('posts/', views.APIPostList.as_view(), name='posts_list'),
-    # path('posts/<int:pk>/', views.APIPostDetail.as_view(), name='posts_detail'),
-
-    # path('posts/', views.PostList.as_view(), name='posts_list'),
-    # path('posts/<int:pk>/', views.PostDetail.as_view(), name='posts_detail'),
-
-    path('posts/', views.PostList2.as_view(), name='posts_list'),
-    path('posts/<int:pk>/', views.PostDetail2.as_view(), name='posts_detail'),
+    path( '', include( router.urls ) )
 ]
